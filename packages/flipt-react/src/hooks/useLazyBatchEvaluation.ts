003051f9a3b0be4d317e32789702774ffc0b3ccd
Lucas Viana <contato@lucasviana.dev>
2021-09-14 16:12:31 -0300

    feat: creates hooks to batch evaluation

---
+++ packages/flipt-react/src/hooks/useLazyBatchEvaluation.ts
@@ -0,0 +1,22 @@
+import { FliptContext } from "@/context/FliptProvider";
+import type EvaluationConfig from "@/types/EvalutationConfig";
+import type { Request } from "@betrybe/flipt-sdk";
+import useTask from "@/utils/hooks/useTask";
+import { useCallback, useContext } from "react";
+
+function useLazyBatchEvaluation(requests: Request[], options: Pick<EvaluationConfig, 'requestId'>) {
+    const fliptContext = useContext(FliptContext);
+
+    if (!fliptContext) {
+        throw new Error('useLazyBatchEvaluation must be used within a FliptContext');
+    }
+
+    const { loading, result, error } = useTask(useCallback(() => 
+        fliptContext.flipt.batchEvaluate(requests, options.requestId), 
+        [requests, options])
+    );
+
+    return { loading, match: result?.responses ?? [], error };
+}
+
+export default useLazyBatchEvaluation;
