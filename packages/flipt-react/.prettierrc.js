d58858cafe97f577ebd5b0b8ab70cbf0d045677a
Lucas Viana <contato@lucasviana.dev>
2021-09-14 16:16:37 -0300

    feat: adds ESLint & Prettier

---
+++ packages/flipt-react/.prettierrc.js
@@ -0,0 +1,10 @@
+// @ts-check
+/* eslint-env node */
+
+/**
+ * An object with Prettier.js options.
+ * @type {import('prettier').Options}
+ */
+ const options = require('../../.prettierrc.js');
+
+ module.exports = options;
