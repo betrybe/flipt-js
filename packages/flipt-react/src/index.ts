cae82930d6d7063bae8c8158017a52f8e50fa64c
Lucas Viana <contato@lucasviana.dev>
2021-09-14 16:13:28 -0300

    feat: exports react components and hooks

---
+++ packages/flipt-react/src/index.ts
@@ -0,0 +1,11 @@
+// React.js Hooks
+export { default as useEvaluation } from './hooks/useEvaluation';
+export { default as useBatchEvaluation } from './hooks/useBatchEvaluation';
+export { default as useLazyEvaluation } from './hooks/useLazyEvaluation';
+export { default as useLazyBatchEvaluation } from './hooks/useLazyBatchEvaluation';
+
+// React.js components
+export { default as Evaluate } from './components/Evaluate';
+
+// Providers
+export { default as FliptProvider } from './context/FliptProvider';
