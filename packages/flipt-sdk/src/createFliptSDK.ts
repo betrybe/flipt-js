69b519ed5ac8488849616ce1c863242eb64a4450
Lucas Viana <contato@lucasviana.dev>
2021-09-17 09:49:52 -0300

    feat: adds abortsignal to evaluation methods

--- packages/flipt-sdk/src/createFliptSDK.ts
+++ packages/flipt-sdk/src/createFliptSDK.ts
@@ -5,16 +5,21 @@ import type FliptConfig from './@types/FliptConfig';
 import type Request from './@types/Request';
 import { BATCH_EVALUATE_ROUTE, EVALUTE_ROUTE } from './routes';
 
+type RequestOptions = {
+  requestId?: string;
+  signal?: AbortSignal | null;
+};
+
 export type FlipSDKInstance = {
   evaluate(
     flagKey: string,
     entityId: string,
     context: Context,
-    requestId?: string | undefined,
+    options: RequestOptions,
   ): Promise<Evalutation<Context>>;
   batchEvaluate(
     requests: Request[],
-    requestId?: string | undefined,
+    options: RequestOptions,
   ): Promise<BatchEvalutationResponse<Context>>;
 };
 
@@ -22,39 +27,38 @@ function createFliptSDK(config: FliptConfig): FlipSDKInstance {
   if (!window.fetch)
     throw new Error("This browser doesn't support window.fetch()");
 
-  const customFetch = (input: RequestInfo, init?: RequestInit) =>
-    fetch(config.uri + input, {
-      headers: {
-        'Content-Type': 'application/json',
-        ...init?.headers,
-      },
-      ...init,
-    });
-
   function evaluate(
     flagKey: string,
     entityId: string,
     context: Context,
-    requestId?: string,
+    options: RequestOptions,
   ) {
-    return customFetch(EVALUTE_ROUTE, {
+    return fetch(config.uri + EVALUTE_ROUTE, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
       method: 'POST',
       body: JSON.stringify({
-        request_id: requestId,
+        request_id: options.requestId,
         flag_key: flagKey,
         entity_id: entityId,
         context,
       } as Request),
+      signal: options.signal,
     }).then<Evalutation<typeof context>>((response) => response.json());
   }
 
-  function batchEvaluate(requests: Request[], requestId?: string) {
-    return customFetch(BATCH_EVALUATE_ROUTE, {
+  function batchEvaluate(requests: Request[], options: RequestOptions) {
+    return fetch(config.uri + BATCH_EVALUATE_ROUTE, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
       method: 'POST',
       body: JSON.stringify({
-        request_id: requestId,
+        request_id: options.requestId,
         requests,
       }),
+      signal: options.signal,
     }).then<BatchEvalutationResponse<typeof requests[number]['context']>>(
       (response) => response.json(),
     );
