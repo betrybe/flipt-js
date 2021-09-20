765598c4d2a1941eed9c61dfc378c9362744c61d
Lucas Viana <contato@lucasviana.dev>
2021-09-20 13:36:16 -0300

    feat (flipt-sdk): exports Evaluation type

--- packages/flipt-sdk/types/index.d.ts
+++ packages/flipt-sdk/types/index.d.ts
@@ -1,4 +1,5 @@
 export { default as createFliptSDK } from './createFliptSDK';
 export type { FlipSDKInstance } from './createFliptSDK';
 export type { default as Request } from './@types/Request';
+export type { default as Evaluation } from './@types/Evaluation';
 //# sourceMappingURL=index.d.ts.map
