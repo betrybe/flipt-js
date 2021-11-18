1ded85f4678abcca2b26f1c7035417a6b96ccb7d
Mateus Talles <mtalles@avantstay.com>
2021-11-18 02:16:05 -0300

    ft: evaluate and batchEvaluate unit tests

--- packages/flipt-sdk/jest.config.js
+++ packages/flipt-sdk/jest.config.js
@@ -8,6 +8,10 @@
 const options = {
   preset: 'ts-jest',
   resolver: 'ts-jest-resolver',
+  testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/setupJest.ts'],
 };
+
+// used 'jsdom' testEnvironment so I can access the window object in the tests
 
 module.exports = options;
