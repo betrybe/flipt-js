2f2edabdf2d54bf48f946c2bdc474c89c4cb3562
Lucas Viana <contato@lucasviana.dev>
2022-02-22 09:19:24 -0300

    fix(flipt-react): removes outdated types from flipt-react

--- packages/flipt-react/types/utils/hooks/useLazyTask.d.ts
+++
@@ -1,8 +0,0 @@
-import type { State as State } from '../types/State';
-/**
- * React.js hook that provides a function to run in a lazy (lazy) way
- * the task and the task's result, loading and error states.
- */
-declare function useLazyTask<T>(task: Task<T>): [() => Promise<void>, State<T>];
-export default useLazyTask;
-//# sourceMappingURL=useLazyTask.d.ts.map
